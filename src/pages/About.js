c634e5a4cfdcd6c74cdb3ce631791e6418d49008
Cfy1010 <cy_one2000@yahoo.fr>
2021-10-11 20:37:17 +0200

    add components sass & assets

---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <Logo />
+      <h1>À propos </h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque quas
+        porro iusto eveniet deleniti beatae ratione accusantium harum tempora
+        odit magnam non eos, ab error asperiores delectus minus ducimus, illo
+        expedita unde. Ullam impedit reprehenderit sunt neque, aliquid, vel at
+        dolores modi temporibus assumenda illo architecto omnis. Labore deleniti
+        recusandae iusto dolores? Perferendis ipsam voluptates et! Asperiores
+        possimus quis omnis!
+      </p>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, in?</p>
+    </div>
+  );
+};
+
+export default About;
